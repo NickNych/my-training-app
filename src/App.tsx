81530ad90988cef24ad8d89882cc08dfaf75c15e
Nick <nikolpaladiy@gmail.com>
2023-09-04 18:35:16 -0400

    init: start SVG in React lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import image from "./images/CardLayer.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Hello</Button>
+{/*      <Card>*/}
+{/*<CardImage></CardImage>*/}
+{/*      </Card>*/}
     </div>
   );
 }
 
 export default App;
+
+const Button =styled.button`
+border-color: white;
+`
+
+// const Card = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   width: 300px;
+//   height: 350px;
+//   border-radius: 15px;
+//   background-color: #FFF;
+//   box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.10);
+//   padding: 10px;
+// `
+//
+//
+// const CardImage =styled.div`
+//   width: 280px;
+//   height: 170px;
+//   border-radius: 10px;
+//   background-image: url("${image}");
+//   background-position: center;
+//   background-size: cover;
+// `
